5204781ca8f646c6e2777535f2b672ca1d07653f
Naidu <sivatejanaidu78@gmail.com>
2025-04-06 14:44:03 +0530

    bkt-2 added

---
+++ uat deploy/s3-bkt2.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id = "rule-1"
+
+    # ... other transition/expiration actions ...
+
+    status = "Enabled"
+  }
+}
